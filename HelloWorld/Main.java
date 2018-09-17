bbc9d765ee71eac2035d8eab8ff697c280b8249c
Karol Lassak <ingwar@ingwar.eu.org>
2018-09-17 10:26:28 +0200

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -5,7 +5,16 @@ public class Main
     public static void main(String[] args) throws Exception
     {
         System.out.println("Hello world");
-        Thread.sleep(400000);
+        
+        Vector v = new Vector();
+        while (true)
+        {
+            byte b[] = new byte[1048576];
+            v.add(b);
+            Runtime rt = Runtime.getRuntime();
+            System.out.println( "free memory: " + rt.freeMemory() );
+            Thread.sleep(400);
+        }
         System.out.println("Hello world done");
     }
 }
