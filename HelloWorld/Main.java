ae39788a1fa9b2446f3d17f51f8b93cb29195a6c
Karol Lassak <ingwar@ingwar.eu.org>
2018-09-17 10:14:59 +0200

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -5,5 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        Thread.sleep(400000);
+        System.out.println("Hello world done");
     }
 }
