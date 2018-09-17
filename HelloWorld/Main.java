a9659006975fe5d8c10b54e3626dc7451dd7d511
Karol Lassak <ingwar@ingwar.eu.org>
2018-09-17 10:16:11 +0200

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -2,7 +2,7 @@ package HelloWorld;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         System.out.println("Hello world");
         Thread.sleep(400000);
